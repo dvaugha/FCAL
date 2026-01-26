fe28dde8a80be8fc6f239759eb49174acc1922fa
dvaugha <dvaughan46@gmail.com>
2026-01-26 15:23:40 -0500

    Update 1/26/2026 3:23 PM

--- update_github.bat
+++ update_github.bat
@@ -1,8 +1,9 @@
 @echo off
 echo Updating GitHub repository...
 git add .
-set /p commit_msg="Enter commit message (default: Update): "
-if "%commit_msg%"=="" set commit_msg=Update
+for /f "tokens=*" %%a in ('powershell -Command "Get-Date -Format 'M/d/yyyy h:mm tt'"') do set datetime=%%a
+set commit_msg=Update %datetime%
+echo Commit Message: "%commit_msg%"
 git commit -m "%commit_msg%"
 git branch -M main
 git push -u origin main
